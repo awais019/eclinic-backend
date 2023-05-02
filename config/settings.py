fba0d035f8904e8d744f99aff8ce97d4aed3fb1b
Muhammad Awais <awaistopper20@gmail.com>
2023-05-02 22:55:51 +0500

    Connected postgres database.

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('USER_NAME'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
