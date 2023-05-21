9f3b915d6e8ee3603bb12d808fe90168e22b9b04
Muhammad Awais <awaistopper20@gmail.com>
2023-05-21 17:25:43 +0500

    Changed admin header and title.

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Eclinic Admin'
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__degbug__/', include(debug_toolbar.urls)),
